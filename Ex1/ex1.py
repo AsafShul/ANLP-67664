2ece606acc528f3a2c4d64d9fde6042a3031714a
asafshul <asafshul96@gmail.com>
2023-05-20 12:22:35 +0300

    ex1 done

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -1,36 +1,113 @@
 # imports:
 import wandb
+import torch
 import numpy as np
-import pandas as pd
+
+
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    set_seed,
+)
 
 import utils
 
 # constants:
-LOG_WITH_WANDB = False
+LOG_WITH_WANDB = True
 MODELS = ['bert-base-uncased', 'roberta-base', 'google/electra-base-generator']
-
-
-def batch_end_callback(trainer):
-    if trainer.iter_num % 10 == 0:
-        print(f"iter_dt {trainer.iter_dt * 1000:.2f}ms; iter {trainer.iter_num}: train loss {trainer.loss.item():.5f}")
-        if LOG_WITH_WANDB:
-            wandb.log({"train_loss": trainer.loss.item()})
-
 
 if __name__ == '__main__':
     # init runtime:
-    utils.init_wandb(LOG_WITH_WANDB)  # todo - add config and name
-    seeds, num_train, num_val, num_test = utils.get_config()
+    utils.init_dirs()
+    run_args, training_args = utils.get_config()
 
-    res = pd.DataFrame(columns=['model', 'seed', 'acc'])
-    for model in MODELS:
-        best_seed = None
-        for seed in seeds:
-            pass  # fine-tune model
-            res.at[len(res)] = [model, seed, acc]
+    # load data:
+    res = {}
 
-    # save results to res.txt file:
+    for seed in run_args.seeds:
+        set_seed(seed)
+        for model_name in MODELS:
+            utils.init_wandb(LOG_WITH_WANDB, config=training_args, name=utils.get_run_name(run_args, model_name, seed))
+            config = AutoConfig.from_pretrained(model_name)
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = AutoModelForSequenceClassification.from_pretrained(model_name, config=config, cache_dir="./data")
+            train_dataset, eval_dataset = utils.get_datasets(run_args, seed, tokenizer)
+
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=train_dataset,
+                eval_dataset=eval_dataset,
+                compute_metrics=utils.get_metric_func('accuracy'),
+                tokenizer=tokenizer,
+            )
+            train_result = trainer.train()
+
+            model.save_pretrained(f'./models/{model_name}_{seed}')
+            model.eval()
+            eval_results = trainer.evaluate(eval_dataset=eval_dataset)
+
+            res[f'{model_name}_{seed}'] = {'train': train_result,
+                                           'eval': eval_results,
+                                           'model': model,
+                                           'tokenizer': tokenizer,
+                                           'model_name': model_name,
+                                           'seed': seed}
+            if LOG_WITH_WANDB:
+                wandb.finish()
+
+    print(res)
+
+    filtered_res = {}
+    training_time, eval_time = [], []
+    best_model_plus_seed_name = None
+    best_acc = -np.inf
+    for model_name in MODELS:
+        model_acc = []
+        for seed in run_args.seeds:
+            acc = res[f'{model_name}_{seed}']['eval']['eval_accuracy']
+            if acc > best_acc:
+                best_acc = acc
+                best_model_plus_seed_name = f'{model_name}_{seed}'
+
+            model_acc.append(acc)
+            training_time.append(res[f'{model_name}_{seed}']['train'].metrics['train_runtime'])
+            eval_time.append(res[f'{model_name}_{seed}']['eval']['eval_runtime'])
+
+        model_acc = np.array(model_acc)
+        filtered_res[model_name] = {'eval_acc_mean': model_acc.mean(),
+                                    'eval_acc_std': model_acc.std()}
+    print('-' * 50)
+    print(filtered_res)
+
+    # write results to res.txt in the format
     with open('res.txt', 'w') as f:
-        for model, model_df in res.groupby('model'):
-            f.write(f'{model},{model_df["acc"].mean():.2f} +- {model_df["acc"].std():.2f}\n')
+        for model_name in MODELS:
+            f.write(
+                f'{model_name},{filtered_res[model_name]["eval_acc_mean"]} +- {filtered_res[model_name]["eval_acc_std"]}\n')
+        f.write('----\n')
+        f.write(f'train time,{np.array(training_time).sum()}\n')
+        f.write(f'predict time,{np.array(eval_time).sum()}\n')
 
+    # load best model and predict:
+    seed = res[best_model_plus_seed_name]['seed']
+    model_name = res[best_model_plus_seed_name]['model_name']
+    model = AutoModelForSequenceClassification.from_pretrained(f'./models/{model_name}_{seed}')
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    test_dataset = utils.get_test_dataset(run_args, seed, tokenizer)
+
+    test_inputs = tokenizer(test_dataset['sentence'], truncation=True, padding=True, return_tensors='pt')
+
+    # Generate predictions
+    model.to('cpu')
+    model.eval()
+    with torch.no_grad():
+        outputs = model(**test_inputs)
+        # outputs = model(test_dataset)
+        predicted_labels = outputs.logits.argmax(dim=1)
+
+    with open('predictions.txt', 'w') as f:
+        for sentence, label in zip(test_dataset['sentence'], predicted_labels):
+            f.write(f'{sentence}###{label}\n')
--- Ex1/utils.py
+++ Ex1/utils.py
@@ -1,24 +1,135 @@
 # imports:
 import os
+import sys
+
 import wandb
-import argparse
 import numpy as np
+from datasets import load_dataset
+from evaluate import load
+
+from dataclasses import dataclass, field
+from transformers import TrainingArguments
+from transformers.hf_argparser import HfArgumentParser
+
+from transformers import EvalPrediction
 
 
-# util functions:
+@dataclass
+class RunArguments:
+    num_seeds: int = field(metadata={'help': 'number of seeds to be used for each model'}, default=1)
+    num_train: int = field(metadata={'help': 'number of samples to be used during training'}, default=-1)
+    num_val: int = field(metadata={'help': 'number of samples to be used during validation'}, default=-1)
+    num_test: int = field(metadata={'help': 'number of samples for which the model will predict a sentiment'},
+                          default=-1)
+
+
 def get_config():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('num_seeds', help='number of seeds to be used for each model', type=int)
-    parser.add_argument('num_train', help='number of samples to be used during training', type=int)
-    parser.add_argument('num_val', help='number of samples to be used during validation', type=int)
-    parser.add_argument('num_test', help='number of samples for which the model will predict a sentiment', type=int)
-    args = parser.parse_args()
+    """
+    Get the config of the experiment
+    :return: seeds, num_train, num_val, num_test
+    """
+    sys.argv.append("--output_dir=./results")
+    parser = HfArgumentParser((RunArguments, TrainingArguments))
+    run_args, training_args = parser.parse_args_into_dataclasses()
+    run_args.seeds = np.arange(run_args.num_seeds)
+    training_args.evaluation_strategy = "epoch"
+    training_args.save_strategy = "epoch"
 
-    return np.arange(args.num_seeds), args.num_train, args.num_val, args.num_test
+    return run_args, training_args
+
+
+def get_run_name(run_args, model_name, seed):
+    """
+    Get the name of the run
+    :param run_args: the run args
+    :param model_name: the name of the model
+    :param seed: the seed
+    :return: the name of the run
+    """
+    return f'{model_name}_seed-{seed}trainSamples-{run_args.num_train}_valSamples-{run_args.num_val}_testSamples-{run_args.num_test}'
+
+
+def init_dirs():
+    """
+    Initialize the folders of the project
+    :return:
+    """
+    work_dirs = ["models", "data", "results"]
+
+    for dir_ in work_dirs:
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+            print(f"Folder '{dir_}' created.")
+        else:
+            print(f"Folder '{dir_}' already exists.")
+
+
+def get_metric_func(metric_name):
+    metric = load(metric_name)
+
+    def compute_metrics(p: EvalPrediction):
+        preds = np.argmax(p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions, axis=1)
+        result = metric.compute(predictions=preds, references=p.label_ids)[metric_name]
+        return {metric_name: result}
+
+    return compute_metrics
+
+
+def get_tokenizer_func(tokenizer):
+    def preprocess_function(examples):
+        # Tokenize the texts
+        result = tokenizer(examples['sentence'], max_length=tokenizer.model_max_length, truncation=True)
+        return result
+
+    return preprocess_function
+
+
+def get_datasets(run_args, seed, tokenizer):
+    raw_datasets = load_dataset('sst2', cache_dir="./data")
+    tokenized_datasets = raw_datasets.map(get_tokenizer_func(tokenizer), batched=True)
+
+    train_dataset = tokenized_datasets['train']
+    eval_dataset = tokenized_datasets['validation']
+
+    if run_args.num_train != -1:
+        train_dataset = tokenized_datasets['train'].shuffle(seed=seed).select(range(run_args.num_train))
+    if run_args.num_val != -1:
+        eval_dataset = tokenized_datasets['validation'].shuffle(seed=seed).select(range(run_args.num_val))
+
+    def preprocess_function(examples):
+        # Tokenize the texts
+        result = tokenizer(examples['sentence'], max_length=512, truncation=True)
+        return result
+
+    train_dataset = train_dataset.map(preprocess_function, batched=True, batch_size=None)
+    eval_dataset = eval_dataset.map(preprocess_function, batched=True, batch_size=None)
+
+    return train_dataset, eval_dataset
+
+
+def get_test_dataset(run_args, seed, tokenizer):
+    raw_datasets = load_dataset('sst2', cache_dir="./data")
+    raw_datasets.set_format(output_all_columns=True)
+    tokenized_datasets = raw_datasets.map(get_tokenizer_func(tokenizer), batched=True, batch_size=100)
+
+    test_dataset = tokenized_datasets['test']
+
+    if run_args.num_test != -1:
+        test_dataset = tokenized_datasets['test'].shuffle(seed=seed).select(range(run_args.num_test))
+
+    return test_dataset
 
 
 def init_wandb(LOG_WITH_WANDB, config='default', name='default'):
+    """
+    Initialize wandb
+    :param LOG_WITH_WANDB: bool - whether to log with wandb
+    :param config: the config to be used for wandb
+    :param name: the name to be used for wandb
+    :return: None
+    """
     if not LOG_WITH_WANDB:
         return
-    wandb.login(key=os.environ['WANDB_API_KEY'], relogin=True)
+
+    wandb.login(key=os.environ.get('WANDB_API_KEY'), relogin=True)
     wandb.init(project="ANLP-ex1", config=config, name=name)
