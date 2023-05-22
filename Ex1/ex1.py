257cec0a41689a0dc89dab01424ab4791de9a151
asafshul <asafshul96@gmail.com>
2023-05-22 18:54:49 +0300

    final ex1

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -1,9 +1,4 @@
 # imports:
-import wandb
-import torch
-import numpy as np
-
-
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
@@ -21,11 +16,11 @@ MODELS = ['bert-base-uncased', 'roberta-base', 'google/electra-base-generator']
 if __name__ == '__main__':
     # init runtime:
     utils.init_dirs()
+    utils.login_wandb(LOG_WITH_WANDB)
     run_args, training_args = utils.get_config()
 
     # load data:
     res = {}
-
     for seed in run_args['seeds']:
         set_seed(seed)
         for model_name in MODELS:
@@ -45,7 +40,8 @@ if __name__ == '__main__':
             )
             train_result = trainer.train()
 
-            model.save_pretrained(f'./models/{model_name}_{seed}')
+            trainer.save_model(utils.format_path(f'models/{model_name}_{seed}'))
+
             model.eval()
             eval_results = trainer.evaluate(eval_dataset=eval_dataset)
 
@@ -55,59 +51,10 @@ if __name__ == '__main__':
                                            'tokenizer': tokenizer,
                                            'model_name': model_name,
                                            'seed': seed}
-            if LOG_WITH_WANDB:
-                wandb.finish()
 
-    print(res)
+            utils.finish_wandb(LOG_WITH_WANDB)
 
-    filtered_res = {}
-    training_time, eval_time = [], []
-    best_model_plus_seed_name = None
-    best_acc = -np.inf
-    for model_name in MODELS:
-        model_acc = []
-        for seed in run_args['seeds']:
-            acc = res[f'{model_name}_{seed}']['eval']['eval_accuracy']
-            if acc > best_acc:
-                best_acc = acc
-                best_model_plus_seed_name = f'{model_name}_{seed}'
+    filtered_res, training_time, eval_time, best_model_plus_seed_name = utils.filter_results(res, run_args, MODELS)
+    utils.save_results_to_file(filtered_res, training_time, eval_time, MODELS)
+    utils.save_test_predictions_to_file(best_model_plus_seed_name, run_args['num_test'])
 
-            model_acc.append(acc)
-            training_time.append(res[f'{model_name}_{seed}']['train'].metrics['train_runtime'])
-            eval_time.append(res[f'{model_name}_{seed}']['eval']['eval_runtime'])
-
-        model_acc = np.array(model_acc)
-        filtered_res[model_name] = {'eval_acc_mean': model_acc.mean(),
-                                    'eval_acc_std': model_acc.std()}
-    print('-' * 50)
-    print(filtered_res)
-
-    # write results to res.txt in the format
-    with open('res.txt', 'w') as f:
-        for model_name in MODELS:
-            f.write(
-                f'{model_name},{filtered_res[model_name]["eval_acc_mean"]} +- {filtered_res[model_name]["eval_acc_std"]}\n')
-        f.write('----\n')
-        f.write(f'train time,{np.array(training_time).sum()}\n')
-        f.write(f'predict time,{np.array(eval_time).sum()}\n')
-
-    # load best model and predict:
-    seed = res[best_model_plus_seed_name]['seed']
-    model_name = res[best_model_plus_seed_name]['model_name']
-    model = AutoModelForSequenceClassification.from_pretrained(f'./models/{model_name}_{seed}')
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    test_dataset = utils.get_test_dataset(run_args, seed, tokenizer)
-
-    test_inputs = tokenizer(test_dataset['sentence'], truncation=True, padding=True, return_tensors='pt')
-
-    # Generate predictions
-    model.to('cpu')
-    model.eval()
-    with torch.no_grad():
-        outputs = model(**test_inputs)
-        # outputs = model(test_dataset)
-        predicted_labels = outputs.logits.argmax(dim=1)
-
-    with open('predictions.txt', 'w') as f:
-        for sentence, label in zip(test_dataset['sentence'], predicted_labels):
-            f.write(f'{sentence}###{label}\n')
--- Ex1/utils.py
+++ Ex1/utils.py
@@ -2,13 +2,27 @@
 import os
 import sys
 
+import torch
+
 import wandb
 import numpy as np
 from evaluate import load
 from datasets import load_dataset
+from transformers import (
+    EvalPrediction,
+    TrainingArguments,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
 
-from transformers import EvalPrediction
-from transformers import TrainingArguments
+# init output path:
+RUNNING_ON_COLAB = '/content' == os.getcwd()
+BASE_PATH = '/content/gdrive/MyDrive/aml' if RUNNING_ON_COLAB else '.'
+print(f'Running on colab: {RUNNING_ON_COLAB}')
+
+
+def format_path(dir_path):
+    return os.path.join(BASE_PATH, dir_path)
 
 
 def get_config():
@@ -18,7 +32,7 @@ def get_config():
     """
     run_args = dict(zip(['num_seeds', 'num_train', 'num_val', 'num_test'], np.array(sys.argv[1:5], dtype=int)))
     run_args['seeds'] = np.arange(run_args['num_seeds'])
-    training_args = TrainingArguments(output_dir="./results", evaluation_strategy="epoch", save_strategy="epoch")
+    training_args = TrainingArguments(output_dir=format_path("results"), evaluation_strategy="epoch", save_strategy="epoch")
 
     return run_args, training_args
 
@@ -40,13 +54,13 @@ def init_dirs():
     :return:
     """
     work_dirs = ["models", "data", "results"]
-
     for dir_ in work_dirs:
-        if not os.path.exists(dir_):
-            os.makedirs(dir_)
-            print(f"Folder '{dir_}' created.")
+        dir_path = format_path(dir_)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+            print(f"Folder '{dir_path}' created.")
         else:
-            print(f"Folder '{dir_}' already exists.")
+            print(f"Folder '{dir_path}' already exists.")
 
 
 def get_metric_func(metric_name):
@@ -63,46 +77,46 @@ def get_metric_func(metric_name):
 def get_tokenizer_func(tokenizer):
     def preprocess_function(examples):
         # Tokenize the texts
-        result = tokenizer(examples['sentence'], max_length=tokenizer.model_max_length, truncation=True)
+        result = tokenizer(examples['sentence'], truncation=True)
         return result
 
     return preprocess_function
 
 
 def get_datasets(run_args, seed, tokenizer):
+    pre_process_func = get_tokenizer_func(tokenizer)
     raw_datasets = load_dataset('sst2', cache_dir="./data")
-    tokenized_datasets = raw_datasets.map(get_tokenizer_func(tokenizer), batched=True)
 
-    train_dataset = tokenized_datasets['train']
-    eval_dataset = tokenized_datasets['validation']
+    train_dataset = raw_datasets['train'].map(pre_process_func, batched=True, batch_size=None)
+    eval_dataset = raw_datasets['validation'].map(pre_process_func, batched=True, batch_size=None)
 
     if run_args["num_train"] != -1:
-        train_dataset = tokenized_datasets['train'].shuffle(seed=seed).select(range(run_args["num_train"]))
+        train_dataset = train_dataset.shuffle(seed=seed).select(range(run_args["num_train"]))
     if run_args["num_val"] != -1:
-        eval_dataset = tokenized_datasets['validation'].shuffle(seed=seed).select(range(run_args["num_val"]))
-
-    def preprocess_function(examples):
-        # Tokenize the texts
-        result = tokenizer(examples['sentence'], max_length=512, truncation=True)
-        return result
-
-    train_dataset = train_dataset.map(preprocess_function, batched=True, batch_size=None)
-    eval_dataset = eval_dataset.map(preprocess_function, batched=True, batch_size=None)
+        eval_dataset = eval_dataset.shuffle(seed=seed).select(range(run_args["num_val"]))
 
     return train_dataset, eval_dataset
 
 
-def get_test_dataset(run_args, seed, tokenizer):
-    raw_datasets = load_dataset('sst2', cache_dir="./data")
-    raw_datasets.set_format(output_all_columns=True)
-    tokenized_datasets = raw_datasets.map(get_tokenizer_func(tokenizer), batched=True, batch_size=100)
+def get_test_dataset(num_test):
+    test_dataset = load_dataset('sst2', cache_dir="./data", split='test')
 
-    test_dataset = tokenized_datasets['test']
-
-    if run_args["num_test"] != -1:
-        test_dataset = tokenized_datasets['test'].shuffle(seed=seed).select(range(run_args["num_test"]))
+    if num_test != -1:
+        test_dataset = test_dataset.select(range(num_test))
 
     return test_dataset
+
+
+def login_wandb(LOG_WITH_WANDB):
+    """
+    Login to wandb
+    :param LOG_WITH_WANDB: bool - whether to log with wandb
+    :return: None
+    """
+    if not LOG_WITH_WANDB:
+        return
+
+    wandb.login()
 
 
 def init_wandb(LOG_WITH_WANDB, config='default', name='default'):
@@ -116,5 +130,70 @@ def init_wandb(LOG_WITH_WANDB, config='default', name='default'):
     if not LOG_WITH_WANDB:
         return
 
-    wandb.login()
-    wandb.init(project="ANLP-ex1", config=config, name=name)
+    wandb.init(project="ANLP-ex1_asafshul_f", config=config, name=name)
+
+
+def finish_wandb(LOG_WITH_WANDB):
+    """
+    Finish wandb
+    :param LOG_WITH_WANDB: bool - whether to log with wandb
+    :return: None
+    """
+    if not LOG_WITH_WANDB:
+        return
+
+    wandb.finish()
+
+
+def filter_results(res, run_args, models):
+    filtered_res = {}
+    training_time, eval_time = [], []
+    best_model_plus_seed_name = None
+
+    best_acc = -np.inf
+    for model_name in models:
+        model_acc = []
+        for seed in run_args['seeds']:
+            acc = res[f'{model_name}_{seed}']['eval']['eval_accuracy']
+            if acc > best_acc:
+                best_acc = acc
+                best_model_plus_seed_name = f'{model_name}_{seed}'
+
+            model_acc.append(acc)
+            training_time.append(res[f'{model_name}_{seed}']['train'].metrics['train_runtime'])
+            eval_time.append(res[f'{model_name}_{seed}']['eval']['eval_runtime'])
+
+        model_acc = np.array(model_acc)
+        filtered_res[model_name] = {'eval_acc_mean': model_acc.mean(),
+                                    'eval_acc_std': model_acc.std()}
+    return filtered_res, training_time, eval_time, best_model_plus_seed_name
+
+
+def save_results_to_file(filtered_res, training_time, eval_time, models):
+    with open(format_path('res.txt'), 'w') as f:
+        for model_name in models:
+            f.write(
+                f'{model_name},{filtered_res[model_name]["eval_acc_mean"]} +- {filtered_res[model_name]["eval_acc_std"]}\n')
+        f.write('----\n')
+        f.write(f'train time,{np.array(training_time).sum()}\n')
+        f.write(f'predict time,{np.array(eval_time).sum()}\n')
+
+
+def save_test_predictions_to_file(best_model_plus_seed_name, num_test):
+    # load best model and predict:
+    model_name = best_model_plus_seed_name.split('_')[0]
+    model = AutoModelForSequenceClassification.from_pretrained(format_path(f'models/{best_model_plus_seed_name}'))
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    test_dataset = get_test_dataset(num_test)
+
+    model.eval()
+    preds = []
+    with torch.no_grad():
+        for sample in test_dataset:
+            tokenized_sample = tokenizer(sample['sentence'], truncation=True, return_tensors='pt')
+            preds.append(model(**tokenized_sample).logits.argmax(dim=1).cpu().numpy()[0])
+
+    print(preds)
+    with open(format_path('predictions.txt'), 'w') as f:
+        for sentence, label in zip(test_dataset['sentence'], preds):
+            f.write(f'{sentence}###{label}\n')
